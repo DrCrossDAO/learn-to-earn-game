8facd4e0ad739bfdf7a0ecb628db29e0519465dd
Nina Kilbride <nina@marsmetro.com>
2024-10-19 11:05:18 -0400

    telegram layout

--- src/app/challenge/1/layout.tsx
+++ src/app/challenge/1/layout.tsx
@@ -1,37 +1,31 @@
-"use client";
+import type { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
+import { getLocale } from 'next-intl/server';
 
-import { ReactNode, useState } from 'react';
-import Popup from './popup'; // Ensure the import path is correct
+import { Root } from '@/components/Root/Root';
+import { I18nProvider } from '@/core/i18n/provider';
 
-// Define the expected prop types
-interface LayoutProps {
-  children: ReactNode;      // ReactNode for children prop
-  helpText: string | ReactNode; // Assuming helpText can be a string or ReactNode
-}
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import 'normalize.css/normalize.css';
+import './_assets/globals.css';
 
-export default function Layout({ children, helpText }: LayoutProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+export const metadata: Metadata = {
+  title: 'Your Application Title Goes Here',
+  description: 'Your application description goes here',
+};
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+export default async function RootLayout({ children }: PropsWithChildren) {
+  const locale = await getLocale();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
-      <div className="w-full max-w-md p-4 md:w-3/5 md:p-8 bg-gray-900 rounded shadow-md relative">
-        {children}
-        <button
-          onClick={openModal}
-          className="ml-4 px-4 py-2 bg-yellow-500 text-black rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 absolute top-0 right-0 mt-2 mr-2 md:mt-4 md:mr-4"
-        >
-          Help
-        </button>
-      </div>
-      {isModalOpen && <Popup helpText={helpText} closePopup={closeModal} />}
-    </div>
+    <html lang={locale}>
+    <body>
+      <I18nProvider>
+        <Root>
+          {children}
+        </Root>
+      </I18nProvider>
+    </body>
+    </html>
   );
 }
