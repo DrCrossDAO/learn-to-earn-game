b03e6c4b21dc4005e12635124c0a9404709be6c3
Nina Kilbride <nina@marsmetro.com>
2024-10-19 12:39:53 -0400

    more layouts

--- src/app/challenge/completion/layout.tsx
+++ src/app/challenge/completion/layout.tsx
@@ -1,33 +1,13 @@
 "use client";
 
-import { useState } from 'react';
-// import AppNavbar from '../components/AppNavbar';
+import 'normalize.css/normalize.css';
+import '../../_assets/globals.css';
 
-
-export default function Layout({ children, helpText }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
+export default function Layout({ children }) {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
-        <div className="w-3/5 max-w-3/5 p-8 bg-gray-900 rounded shadow-md relative">
-          {children}
-          <button
-            onClick={openModal}
-            className="ml-4 px-4 py-2 bg-yellow-500 text-black rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 absolute top-0 right-0 mt-4 mr-4"
-          >
-            Help
-          </button>
-        </div>
-        
+    <div className="min-h-screen bg-gray-800 text-lightText flex items-center justify-center"> {/* Dark background and light text */}
+      <div className="w-full max-w-lg p-6 bg-gray-900 rounded-lg shadow-lg text-center"> {/* Slightly lighter background for content */}
+        {children}
       </div>
     </div>
   );
