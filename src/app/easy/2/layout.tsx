507ddbb4aed2a2d2fc9bc2feeff78b67fc9e8630
Nina Kilbride <nina@marsmetro.com>
2024-10-19 12:36:53 -0400

    layouts

--- src/app/easy/2/layout.tsx
+++ src/app/easy/2/layout.tsx
@@ -1,33 +1,14 @@
 "use client";
 
-import { useState } from 'react';
-// import AppNavbar from '../components/AppNavbar';
-import Popup from './popup';
+import 'normalize.css/normalize.css';
+import '../../_assets/globals.css';
 
-export default function Layout({ children, helpText }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
+export default function Layout({ children }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
-      {/* Adjusted width and padding for responsiveness */}
-      <div className="w-full max-w-md p-4 md:w-3/5 md:p-8 bg-gray-900 rounded shadow-md relative">
+    <div className="min-h-screen bg-gray-800 text-lightText flex items-center justify-center"> {/* Dark background and light text */}
+      <div className="w-full max-w-lg p-6 bg-gray-900 rounded-lg shadow-lg text-center"> {/* Slightly lighter background for content */}
         {children}
-        <button
-          onClick={openModal}
-          className="ml-4 px-4 py-2 bg-yellow-500 text-black rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 absolute top-0 right-0 mt-2 mr-2 md:mt-4 md:mr-4"
-        >
-          Help
-        </button>
       </div>
-      {isModalOpen && <Popup helpText={helpText} closePopup={closeModal} />}
     </div>
   );
 }
